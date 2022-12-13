31632076d23d429491cf139e7dd8dcd5ef1faa74
Luis Tellez <luis@luistellez.com>
2022-12-13 15:01:31 -0800

    feat: add email and password to user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -24,6 +24,15 @@ const userSchema = new mongoose.Schema({
       },
     },
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("user", userSchema);
