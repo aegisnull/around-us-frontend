ded19b55f4fd388c569098341e9fa4771a9cab53
Luis Tellez <luis@luistellez.com>
2022-12-13 17:48:16 -0800

    refactor: remove required and add default data

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,19 +6,19 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: "Jacques Cousteau",
   },
 
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: "Explorador",
   },
 
   avatar: {
     type: String,
-    required: true,
+    default: "https://pictures.s3.yandex.net/resources/avatar_1604080799.jpg",
     validate: {
       validator: (value) => validator.isURL(value),
       message: "La URL no es válida",
