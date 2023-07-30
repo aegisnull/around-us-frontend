730e56739d7ff3af53f0c51fd8684dde7a560793
Luis Tellez <luis@luistellez.com>
2023-07-30 11:25:46 -0700

    🔧 chore(.eslintrc.json): disable consistent-return rule to allow functions to have inconsistent return statements 🐛 fix(app.js): add missing semicolons and improve code formatting for better readability ✨ feat(app.js): add support for CORS and enable JSON parsing for incoming requests 🔧 chore(app.js): remove unnecessary comments and improve code formatting for better readability 🔧 chore(app.js): remove unnecessary comments and improve code formatting for better readability ✨ feat(app.js): add routes for cards and users ✨ feat(app.js): add routes for signin and signup 🔧 chore(app.js): export app and server modules for testing purposes

--- backend/app.js
+++ backend/app.js
@@ -1,51 +1,51 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
 // inclúyelos antes de otras rutas
-app.use(cors())
-app.options('*', cors()) // habilitar las solicitudes de todas las rutas
+app.use(cors());
+app.options('*', cors()); // habilitar las solicitudes de todas las rutas
 
 // express.json() is a method to recognize the incoming Request Object as a JSON.
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Database connection function to MongoDB Atlas cluster using mongoose
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL)
-    console.log('Connected to MongoDB')
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log('Connected to MongoDB');
   } catch (error) {
-    console.log('Failed to connect to MongoDB', error)
-    process.exit(1) // Exit the application on connection error
+    console.log('Failed to connect to MongoDB', error);
+    process.exit(1); // Exit the application on connection error
   }
-}
+};
 
-const usersRouter = require('./routes/users')
-const cardRouter = require('./routes/cards')
+const usersRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
 
-app.use('/cards', cardRouter)
-app.use('/users', usersRouter)
+app.use('/cards', cardRouter);
+app.use('/users', usersRouter);
 
 // set route for Non-existent address or localhost:3000
 app.use('/', (req, res) => {
-  res.status(404).send({ message: 'Recurso solicitado no encontrado' })
-})
+  res.status(404).send({ message: 'Recurso solicitado no encontrado' });
+});
 
 // Port number for the server to listen on
-const { PORT } = process.env
+const { PORT } = process.env;
 const server = app.listen(PORT, () => {
-  console.log(`Server is running at port ${PORT}`)
-  connectDB()
-})
+  console.log(`Server is running at port ${PORT}`);
+  connectDB();
+});
 
-const { login, createUser } = require('./controllers/users')
+const { login, createUser } = require('./controllers/users');
 
-app.post('/signin', login)
-app.post('/signup', createUser)
+app.post('/signin', login);
+app.post('/signup', createUser);
 
-module.exports = { app, server }
+module.exports = { app, server };
