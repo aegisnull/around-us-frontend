c03f6f3b3c634f11433c38f71bb98354516774b8
Luis Tellez <luis@luistellez.com>
2023-07-30 09:28:44 -0700

    🐛 fix(auth.js): handle authorization failure by sending a 401 response with an error message 🔒 chore(auth.js): use JWT secret from environment variable instead of hardcoding it 📦 chore(package.json): update dependencies and description of the project

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,21 +1,22 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const auth = (req, next) => {
+const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new Error('Authorization failed');
+    return res.status(401).send({ message: 'Authorization failed' });
   }
+
   const token = authorization.replace('Bearer ', '');
-  let payload;
   try {
-    payload = jwt.verify(token, 'encoding-string');
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = payload;
+    next();
   } catch (err) {
-    throw new Error('Authorization failed');
+    return res.status(401).send({ message: 'Authorization failed' });
   }
-  req.user = payload;
-  next();
 };
 
 module.exports = {
-  auth,
+  auth
 };
